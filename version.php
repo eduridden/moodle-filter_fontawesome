b372b0d0dd55c538e85b55c75fcfe01048c72e2d
Julian Ridden <julian@moodleman.net>
2013-11-21 10:18:05 +1100

    Update version.php

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013110200;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013112100;            // The current plugin version (Date: YYYYMMDDXX)
 $plugin->maturity = MATURITY_STABLE;        // this version's maturity level.
-$plugin->release = '1.1 (Build: 20131102)';
+$plugin->release = '2.0 (Build: 20131121)';
 $plugin->requires  = 2013050100;           // Requires this Moodle version
 $plugin->component = 'filter_fontawesome'; // Full name of the plugin (used for diagnostics)
